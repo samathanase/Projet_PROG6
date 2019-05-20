3345ea4bfc284f08d98b4b708d4c98257b773f87
samibk201 <45363113+samibk201@users.noreply.github.com>
2019-05-20 14:56:44 +0200

    Update GridView.java

--- View/GridView.java
+++ View/GridView.java
@@ -5,6 +5,10 @@ import javafx.scene.canvas.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 import Modele.*;
+import fanorona_prog06.PvP_board;
+import static fanorona_prog06.PvP_board.en_cours;
+import static fanorona_prog06.PvP_board.p1;
+import static fanorona_prog06.PvP_board.p2;
 import javafx.scene.image.Image;
 public class GridView{
 
@@ -32,6 +36,7 @@ public class GridView{
 	
 
 	public void draw(Canvas cnv){
+            int l_p = 0,l_c = 0;
 		GraphicsContext gc = cnv.getGraphicsContext2D();
 
 
@@ -43,7 +48,7 @@ public class GridView{
 
 
 		//Affichage des lignes
-		for(int i = 0; i < m_game.colonne(); i++){
+		/*for(int i = 0; i < m_game.colonne(); i++){
 			gc.strokeLine((i+0.5)*tileSizeX,0.5*tileSizeY,(i+0.5)*tileSizeX,(m_game.ligne()-0.5)*tileSizeY);
 			
 		}
@@ -62,8 +67,15 @@ public class GridView{
 				}
 			}
 			
-		}
-
+		}*/
+                //Affichage du nom du joueur qui a la main
+                if (m_game.joueur==1){
+                    en_cours.setText(p1);
+                }else{
+                    en_cours.setText(p2);
+                }
+                
+                
 		//Affichage des select 
 		if(m_game.pionSelectionne() != null){
 			for(int x = 0; x < m_game.colonne(); x++){
@@ -73,14 +85,27 @@ public class GridView{
 						//gc.setFill(Color.BLACK);
                                                 gc.setFill(Color.web("06ff00"));
 						gc.fillOval((x+0.25)*tileSizeX-thickness,(y+0.25)*tileSizeY-thickness,tileSizeX/2+2*thickness,tileSizeY/2+2*thickness);
-
+                                                l_p=y;
+                                                l_c=x;
 					}
 				}
 			}
 		}
+                //Affichages des cases accessibles
+                Coordonnees crd = new Coordonnees(l_p,l_c);
+                Image ac = new Image(getClass().getResourceAsStream("pions/accessible.png"));
+                for(int x = 0; x < m_game.colonne(); x++){
+                    for(int y = 0; y < m_game.ligne(); y++){
+                        if(m_game.casesAccessibles(crd).contains(new Coordonnees(y,x))){
+                            gc.drawImage(ac,(x+0.25)*tileSizeX,(y+0.25)*tileSizeY, tileSizeX/2, tileSizeY/2);
+                        }
+                    }         
+                }
+                
 
 		Image pb = new Image(getClass().getResourceAsStream("pions/pion_b.png"));
                 Image pg = new Image(getClass().getResourceAsStream("pions/pion_r.png"));
+                
 
 		//Affichage des pions
 		for(int x = 0; x < m_game.colonne(); x++){
@@ -93,39 +118,12 @@ public class GridView{
 					//gc.setFill(Color.BLUE);
 					gc.drawImage(pb,(x+0.25)*tileSizeX,(y+0.25)*tileSizeY, tileSizeX/2, tileSizeY/2);
 				}
-				if(!m_game.libre(y,x)){
-					//gc.fillOval((x+0.25)*tileSizeX,(y+0.25)*tileSizeY,tileSizeX/2,tileSizeY/2);
-				}
+                                //Renvoie la liste des cases accessibles du pion donnÃ©
+    //                          public ArrayList<Coordonnees> casesAccessibles(int l, int c)
 			}
+
 		}
 
 	}
 
 }
-/*
-class _Grille extends Partie {
-
-	public _Grille(){
-	}
-
-	@Override
-	public int ligne(){
-		return tab.length;
-	}
-
-	@Override
-	public int colonne(){
-		if(tab[0] != null){
-			return tab[0].length;
-		}
-		else{
-			return 0;
-		}
-
-	}
-
-	public int at(int x, int y){
-		return tab[x][y];
-	}
-
-}*/
