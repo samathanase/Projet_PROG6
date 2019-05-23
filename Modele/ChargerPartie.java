464f0f28cda6d795f6987ae787daeec6ba3df3a0
Damicap <damicap38@gmail.com>
2019-05-23 10:53:36 +0200

    Amélioration de charger sauvegarder

--- Modele/ChargerPartie.java
+++ Modele/ChargerPartie.java
@@ -1,15 +1,19 @@
 package Modele;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import Configuration.Configuration;
 
 public class ChargerPartie {
     String nomFichier;
 
-    //prend le chemin vers la sauvegarde
+    // prend le nom du fichier à charger
+    // La sauvegarde se trouve dans répertoire courant/.fanorona/sauvegarde/
     public ChargerPartie(String nomFichier) {
-        this.nomFichier = nomFichier;
+        String chemin = Configuration.instance().repertoireCourant();
+        this.nomFichier = chemin + "/sauvegarde/" + nomFichier;
     }
 
     public Partie charger() {
@@ -20,11 +24,28 @@ public class ChargerPartie {
             fichier = new FileInputStream(nomFichier);
             fluxObjet = new ObjectInputStream(fichier);
             partie = (Partie) fluxObjet.readObject();
-            Configuration.instance().logger().info("Partie chargée depuis: "+nomFichier);
+            Configuration.instance().logger().info("Partie chargée depuis: " + nomFichier);
             fichier.close();
+            System.out.println("Joueur:" + partie.joueur());
         } catch (Exception e) {
-            Configuration.instance().logger().warning("Impossible de charger la partie: "+nomFichier);
+            Configuration.instance().logger().warning("Impossible de charger la partie: " + nomFichier);
         }
-        return  partie;
+        return partie;
     }
+
+    // Retourne la liste des parties sauvegardées
+    public static String [] listeSauvegarde() {
+        String dossierNom = Configuration.instance().repertoireCourant() + "/sauvegarde/";
+        File dossier = new File(dossierNom);
+
+        File[] listeFichier = dossier.listFiles(); //la liste des fichiers
+        String [] nomFichiers = new String [listeFichier.length]; //la liste des noms de fichier
+        int i = 0;
+        for (File f : listeFichier) {
+            nomFichiers [i] = f.getName();
+            i++;
+        }
+        return nomFichiers;
+    }
+
 }
